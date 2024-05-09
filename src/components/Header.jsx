35aab45caa75aaf0e6b40a2a411a47e1f9aa80d1
ewsn0825 <ewsn0825@naver.com>
2024-05-10 00:35:38 +0900

    feat : header 임시 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import logo from "@/assets/logo.svg";
 import { Link } from "react-router-dom";
+import search from "@/assets/icon/search.svg";
 
 function Header() {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center mx-24">
       <Link to="/">
         <h1>
-          <img src={logo} alt="데브 메이트 바로가기" className="w-28" />
+          <img src={logo} alt="데브 메이트 바로가기" className="w-36" />
         </h1>
       </Link>
 
-      <input type="search" className="border rounded-full" />
+      <div className="relative">
+        <label htmlFor="search">
+          <img src={search} alt="검색" className="absolute" />
+        </label>
+        <input id="search" type="search" className="border rounded-full" />
+      </div>
     </div>
   );
 }
