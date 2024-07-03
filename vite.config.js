8f2c4b714b92f410df6e8844137b608bf9f28886
minsik2434 <minsik2434@gmail.com>
2024-07-03 17:36:33 +0900

    Chore : 설정 변경

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import svgr from "vite-plugin-svgr";
+import viteCompression from "vite-plugin-compression";
 
 const __dirname = "src";
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      deleteOriginFile: false,
+      threshold: 10240,
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
+    svgr(),
+  ],
   resolve: {
     // 절대 경로 설정
     alias: [{ find: "@", replacement: resolve(__dirname) }],
