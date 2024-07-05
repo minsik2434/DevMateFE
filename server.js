aad70e88fb1f37fb375f4848bd8cacf263fc26fb
ewsn0825 <ewsn0825@naver.com>
2024-07-05 21:47:24 +0900

    chore : 배포 설정

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(3000, function () {
+  console.log("App is listening on port 3000!");
+ 
+});
+
+
+export default app;
